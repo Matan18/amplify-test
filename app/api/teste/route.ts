5fd25af609366ac7fed4d14df3c17a48c30c31da
Mateus Andriola <mateusandriola18@gmail.com>
2025-02-20 15:48:32 -0300

    add origin as returning object

--- app/api/teste/route.ts
+++ app/api/teste/route.ts
@@ -1,5 +1,5 @@
-const data = { message: process.env.DR_CHRONO_CLIENT_ID };
-
-export function GET() {
+export function GET(request: Request) {
+  const { origin } = new URL(request.url);
+  const data = { origin, message: process.env.DR_CHRONO_CLIENT_ID };
   return new Response(JSON.stringify(data), { status: 200 });
 }
