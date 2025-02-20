f438cc34974c81c463e245d61ecaee97d75ffe36
Mateus Andriola <mateusandriola18@gmail.com>
2025-02-20 16:08:00 -0300

    loggin request

--- app/api/teste/route.ts
+++ app/api/teste/route.ts
@@ -1,4 +1,5 @@
 export function GET(request: Request) {
+  console.log(request);
   const { origin } = new URL(request.url);
   const data = { origin, message: process.env.DR_CHRONO_CLIENT_ID };
   return new Response(JSON.stringify(data), { status: 200 });
