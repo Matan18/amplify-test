3f8351feab94e7f6ca9bcc7752b4865477121259
Mateus Andriola <mateusandriola18@gmail.com>
2025-02-20 14:43:16 -0300

    update env name

--- app/api/teste/route.ts
+++ app/api/teste/route.ts
@@ -1,4 +1,4 @@
-const data = { message: process.env.MESSAGE };
+const data = { message: process.env.DR_CHRONO_CLIENT_ID };
 
 export function GET() {
   return new Response(JSON.stringify(data), { status: 200 });
