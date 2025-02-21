689193dafbb6df86cc5646e809fd21bbd4a9a073
Mateus Andriola <mateusandriola18@gmail.com>
2025-02-21 13:40:30 -0300

    update teste route

--- app/api/redirect/route.ts
+++ app/api/redirect/route.ts
@@ -1,5 +1,5 @@
 export function GET(request: Request) {
-  const redirectUrl = `https://develop.d1tibgpadd1t33.amplifyapp.com/api/teste`;
+  const redirectUrl = `https://develop.d1ve6l3zrsm3pf.amplifyapp.com/api/teste`;
 
   return Response.redirect(redirectUrl, 301);
 }
