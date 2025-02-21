73890d47dacfc8ce05848c437dece50edf00dc96
Mateus Andriola <mateusandriola18@gmail.com>
2025-02-21 11:15:47 -0300

    fix redirect

--- app/api/redirect/route.ts
+++ app/api/redirect/route.ts
@@ -1,5 +1,4 @@
 export function GET(request: Request) {
-  const host = request.headers.get("host");
   const redirectUrl = `https://develop.d1tibgpadd1t33.amplifyapp.com/api/teste`;
 
   return Response.redirect(redirectUrl, 301);
