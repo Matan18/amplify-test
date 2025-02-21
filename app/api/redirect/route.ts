9367e95c640ac56cd39d5dd85f4123724d2b19af
Mateus Andriola <mateusandriola18@gmail.com>
2025-02-21 10:46:08 -0300

    adding redirect

--- app/api/redirect/route.ts
+++ app/api/redirect/route.ts
@@ -1,6 +1,6 @@
 export function GET(request: Request) {
   const host = request.headers.get("host");
-  const redirectUrl = `https://${host}/api/teste`;
+  const redirectUrl = `http://localhost:3000/api/teste`;
 
   return Response.redirect(redirectUrl, 301);
 }
